c7545317d0c710cd66bb90732bd847bc07871a2d
awoo-civ <17804892+awoo-civ@users.noreply.github.com>
2022-06-10 21:23:49 +0000

    add an accurate snitch placement helper

--- common/src/main/java/gjum/minecraft/civ/snitchmod/common/SnitchMod.java
+++ common/src/main/java/gjum/minecraft/civ/snitchmod/common/SnitchMod.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
@@ -44,10 +45,19 @@ public abstract class SnitchMod {
 			"category.snitchmod"
 	);
 
+	private static final KeyMapping previewSnitchFieldKey = new KeyMapping(
+			"key.snitchmod.togglePreviewSnitchFieldKey",
+			InputConstants.Type.KEYSYM,
+			GLFW.GLFW_KEY_N,
+			"category.snitchmod"
+	);
+
 	private static SnitchMod INSTANCE;
 
 	public boolean rangeOverlayVisible = false;
 	public boolean placementHelperVisible = false;
+	@Nullable
+	public Snitch snitchFieldToPreview = null;
 
 	@Nullable
 	private SnitchesStore store;
@@ -65,6 +75,7 @@ public abstract class SnitchMod {
 		registerKeyBinding(openGuiKey);
 		registerKeyBinding(toggleOverlayKey);
 		registerKeyBinding(togglePlacementKey);
+		registerKeyBinding(previewSnitchFieldKey);
 	}
 
 	public abstract void registerKeyBinding(KeyMapping mapping);
@@ -131,7 +142,106 @@ public abstract class SnitchMod {
 			logToChat(new TextComponent(
 					"Placement helper " + (placementHelperVisible ? "visible" : "hidden")));
 		}
+		while (previewSnitchFieldKey.consumeClick()) {
+			Optional<Snitch> optNearestSnitch = streamNearbySnitches(mc.player.position(), 2*23)
+				.filter(Snitch::isAlive)
+				.findFirst();
+			if (optNearestSnitch.isEmpty()) {
+				snitchFieldToPreview = null;
+				logToChat(new TextComponent("No nearby snitches to base a field preview on"));
+				break;
+			}
+			Snitch nearestSnitch = optNearestSnitch.get();
+			BlockPos previewPos = transposeSnitchFieldPositionByDirection(
+				nearestSnitch.getPos(),
+				mc.player.getYRot(),
+				mc.player.getXRot());
+			if (previewPos == null) {
+				// TODO log this
+				logToChat(new TextComponent("Internal error"));
+				break;
+			}
+			snitchFieldToPreview = new Snitch(
+				new WorldPos(
+					nearestSnitch.getPos().getServer(),
+					nearestSnitch.getPos().getWorld(),
+					previewPos.getX(),
+					previewPos.getY(),
+					previewPos.getZ()));
+			logToChat(new TextComponent("Showing a snitch field preview"));
+		}
 		// TODO if block pos changed -> if pos inside snitch range not in before -> send jainfo -> mark refreshed
+	}
+
+	private @Nullable BlockPos transposeSnitchFieldPositionByDirection(
+			@NotNull
+			BlockPos fieldCenter,
+			double yaw,
+			double pitch) {
+		int x = fieldCenter.getX();
+		int y = fieldCenter.getY();
+		int z = fieldCenter.getZ();
+		yaw = ((double) yaw + 180) % 360;
+		if (yaw < 0) {
+			yaw += 360;
+		}
+
+		// Straight up
+		if (pitch <= -60) {
+			return new BlockPos(x, y+23, z);
+		// Bit lower
+		} else if ((pitch > -60) && (pitch <= -30)) {
+			y += 22;
+		// Middle
+		} else if ((pitch > -30) && (pitch <= 30)) {
+			// North
+			if ((yaw >= 337.5) || (yaw < 22.5)) {
+				return new BlockPos(x, y, z-23);
+			// East
+			} else if ((yaw >= 67.5) && (yaw < 112.5)) {
+				return new BlockPos(x+23, y, z);
+			// South
+			} else if ((yaw >= 157.5) && (yaw < 202.5)) {
+				return new BlockPos(x, y, z+23);
+			// West
+			} else if ((yaw >= 247.5) && (yaw < 292.5)) {
+				return new BlockPos(x-23, y, z);
+			}
+		// Bit lower
+		} else if ((pitch > 30) && (pitch <= 60)) {
+			y -= 22;
+		// Straight down
+		} else if (pitch >= 60) {
+			return new BlockPos(x, y-23, z);
+		}
+
+		// North
+		if ((yaw >= 337.5) || (yaw < 22.5)) {
+			return new BlockPos(x, y, z-22);
+		// North East
+		} else if ((yaw >= 22.5) && (yaw < 67.5)) {
+			return new BlockPos(x+22, y, z-22);
+		// East
+		} else if ((yaw >= 67.5) && (yaw < 112.5)) {
+			return new BlockPos(x+22, y, z);
+		// South East
+		} else if ((yaw >= 112.5) && (yaw < 157.5)) {
+			return new BlockPos(x+22, y, z+22);
+		// South
+		} else if ((yaw >= 157.5) && (yaw < 202.5)) {
+			return new BlockPos(x, y, z+22);
+		// South West
+		} else if ((yaw >= 202.5) && (yaw < 247.5)) {
+			return new BlockPos(x-22, y, z+22);
+		// West
+		} else if ((yaw >= 247.5) && (yaw < 292.5)) {
+			return new BlockPos(x-22, y, z);
+		// North West
+		} else if ((yaw >= 292.5) && (yaw < 337.5)) {
+			return new BlockPos(x-22, y, z-22);
+		}
+
+		return null;
 	}
 
 	/**
